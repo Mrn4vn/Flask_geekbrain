2567946b3dfd5e2c4fbec6bbf7c7e0e3bba317f8
Mrn4vn <70034865+Mrn4vn@users.noreply.github.com>
2025-09-17 17:02:02 +0300

    Update app.py

--- blog/app.py
+++ blog/app.py
@@ -9,3 +9,24 @@ def index():
 @app.route("/greet/<name>")
 def greet_name(name: str):
   return f"Hey, {name}"
+
+@app.route("/user/")
+def read_user():
+  name = request.args.get("name")
+  surname = request.args.get("surname")
+  return f"User {name or '[no name]'} {surname or '[no surname]'}"
+
+@app.route("/status/", methods=["GET", "POST"])
+def custom_status_code():
+  if request.method =="GET":
+    return "that was get"
+
+  print("raw bytes data:", request.data)
+
+  if request.form and "code" in request.form:
+    return "code from form", request.form["code"]
+
+  if request.json and "code" in request.json:
+    return "code from json", request.json["code"]
+
+  return "", 204
