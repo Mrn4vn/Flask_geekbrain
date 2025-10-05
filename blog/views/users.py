370dd30984030adb3f4838b9b5900abda358630a
Mrn4vn <70034865+Mrn4vn@users.noreply.github.com>
2025-10-05 22:33:29 +0300

    Update users.py

--- blog/views/users.py
+++ blog/views/users.py
@@ -24,4 +24,10 @@ def user_details(user_id: int):
 ...
 
 @users_app.route("/<int:user_id>/", endpoint="details")
-...
+def user_details(user_id: int):
+  try:
+    user_name = USERS[user_id]
+  except KeyError:
+    raise NotFound(f"User #{user_id} and love doesn't exist")
+  return render_template("users/details.html", user_id=user_id, user_name=user_name)
+
