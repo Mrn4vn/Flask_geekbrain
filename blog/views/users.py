e9026d6cfdf016ddc736573077cd3dba682b611f
Mrn4vn <70034865+Mrn4vn@users.noreply.github.com>
2025-10-09 17:38:58 +0300

    Update users.py

--- blog/views/users.py
+++ blog/views/users.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 from werkzeug.exeptions import NotFound
+from sqlalchemy import Column, Integer, String, Boolean
+from blog.models.database import db
 
 users_app = Blueprint("users_app", __name__)
 USERS = {
@@ -19,3 +21,11 @@ def user_details(user_id: int):
   except KeyError:
     raise NotFound(f"User #{user_id} and love doesn't exist")
   return render_template("users/details.html", user_id=user_id, user_name=user_name)
+
+class User(db.Model):
+  id = Column(Integer, primary_key=True)
+  username = Column(String(80), unique=True, nullable=False)
+  is_staff = Column(Boolean, nullable=False, default=False)
+
+  def __repr__(self):
+    return f"<User #{self.id} {self.username!r}>"
